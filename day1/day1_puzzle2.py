123346275b3e651b49e56d3def04745ed8d0aac2
test <77847315+wqkjfi87@users.noreply.github.com>
2023-12-01 22:51:52 +0000

    day 1 problem 2

---
+++ day1/day1_puzzle2.py
@@ -0,0 +1,31 @@
+import re
+
+nums = {
+    'one':'1', 
+    'two': '2',
+    'three':'3',
+    'four': '4',
+    'five':'5',
+    'six':'6',
+    'seven':'7',
+    'eight':'8',
+    'nine':'9',
+}
+decoded = []
+
+with open('day1data.txt', 'r') as f:
+    for line in f:
+        index_hash = {}
+        for digit in re.finditer('\d', line):
+            index_hash[digit.start()] = line[digit.start()]
+        for key in nums.keys():
+            if key in line:
+                if line.find(key) != line.rfind(key):
+                    index_hash[line.find(key)] = nums[key]
+                    index_hash[line.rfind(key)] = nums[key]
+                else:
+                    index_hash[line.find(key)] = nums[key]
+                    
+        sorted_indexs = sorted(list(index_hash))
+        decoded.append(int(index_hash[sorted_indexs[0]] + index_hash[sorted_indexs[-1]]))
+print(sum(decoded))
